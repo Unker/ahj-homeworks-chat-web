c98aa9a7607d5dbdc4867a5a64ef2ce40212b875
unker <kfi.unknown@gmail.com>
2023-09-12 23:59:25 +0300

    get users names via sse

--- src/js/app.js
+++ src/js/app.js
@@ -1,152 +1,120 @@
-const chatBtn = document.querySelector('.chat-widget__side')
-const chatWidget = document.querySelector('.chat-widget')
+// const chatBtn = document.querySelector('.chat-widget__side')
 // const input = document.querySelector('.chat-widget__input');
 // const messages = document.querySelector('.chat-widget__messages');
 // const chatContainer = document.querySelector('.chat-widget__messages-container');
 
-chatBtn.addEventListener('click', (e) => {
-  chatWidget.classList.add('chat-widget_active');
-});
+// chatBtn.addEventListener('click', (e) => {
+//   chatWidget.classList.add('chat-widget_active');
+// });
 
-
-
-const modal = document.querySelector('.modal');
-const nicknameInput = modal.querySelector('.nickname-input');
-const submitButton = modal.querySelector('.nickname-submit');
-const errorText = modal.querySelector('.error-text');
-
-// Открываем модальное окно при загрузке страницы
-window.addEventListener('load', () => {
-  modal.style.display = 'flex';
-});
-
-// Обработчик события для кнопки "Отправить"
-submitButton.addEventListener('click', async () => {
-  const nickname = nicknameInput.value;
-
-  console.log(nickname)
-
-  // Отправляем никнейм на сервер для проверки доступности
-  const isNicknameAvailable = await checkNicknameAvailability(nickname);
-  if (isNicknameAvailable instanceof Error) {
-    errorText.textContent = 'Сервер не доступен';
-    errorText.style.display = 'block';
-  } else if (isNicknameAvailable) {
-    // Если никнейм доступен, открываем окно чата
-    errorText.style.display = 'none';
-    modal.style.display = 'none';
-    chatWidget.classList.add('chat-widget_active');
-  } else {
-    // Иначе сообщаем пользователю, что никнейм занят
-    errorText.textContent = 'Никнейм занят. Пожалуйста, выберите другой.';
-    errorText.style.display = 'block';
-  }
-
-});
-
-url = 'http://localhost:7070'
-
-// Функция для отправки запроса на сервер для проверки доступности никнейма
-async function checkNicknameAvailability(nickname) {
-  try {
-    const response = await fetch(`${url}/check-nickname/?nickname=${nickname}`, {
-      method: 'POST',
-    });
-
-    if (!response.ok) {
-      throw new Error('Ошибка при проверке никнейма');
-    }
-
-    const data = await response.json();
-    return data.available;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-}
 
 
 
 
-class SubscriptionApi {
+url = 'http://localhost:7070'
+
+
+
+
+
+
+class ChatApi {
   constructor(apiUrl) {
     this.apiUrl = apiUrl;
+    this.nickName = undefined;
+    this.chatWidget = document.querySelector('.chat-widget')
+
+    this.modal = document.querySelector('.modal');
+    this.nicknameInput = modal.querySelector('.nickname-input');
+    this.submitButton = modal.querySelector('.nickname-submit');
+    this.errorText = modal.querySelector('.error-text');
+
+    this.#getUserNameModal();
   }
 
-  async add(user) {
-    const request = fetch(this.apiUrl + 'subscriptions/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user),
+  #getUserNameModal() {
+    // Открываем модальное окно при загрузке страницы
+    window.addEventListener('load', () => {
+      this.modal.style.display = 'flex';
     });
 
-    const result = await request;
+    // Обработчик события для кнопки "Отправить"
+    this.submitButton.addEventListener('click', async () => {
+      const nickname = this.nicknameInput.value;
 
-    if (!result.ok) {
-      console.error('Ошибка');
+      console.log(nickname)
 
-      return;
-    }
+      // Отправляем никнейм на сервер для проверки доступности
+      const isNicknameAvailable = await this.checkNicknameAvailability(nickname);
+      if (isNicknameAvailable instanceof Error) {
+        this.errorText.textContent = 'Сервер не доступен';
+        this.errorText.style.display = 'block';
+      } else if (isNicknameAvailable) {
+        // Если никнейм доступен, открываем окно чата
+        this.errorText.style.display = 'none';
+        this.modal.style.display = 'none';
+        this.chatWidget.classList.add('chat-widget_active');
+        this.nickName = nickname;
+      } else {
+        // Иначе сообщаем пользователю, что никнейм занят
+        this.errorText.textContent = 'Никнейм занят. Пожалуйста, выберите другой.';
+        this.errorText.style.display = 'block';
+      }
 
-    const json = await result.json();
-    const status = json.status;
-
-    console.log(status);
-  }
-
-  async remove(user) {
-    const query = 'subscriptions/' + encodeURIComponent(user.phone);
-
-    const request = fetch(this.apiUrl + query, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      },
     });
 
-    const result = await request;
-
-    if (!result.ok) {
-      console.error('Ошибка!');
-
-      return;
-    }
-
-    const json = await result.json();
-    const status = json.status;
-
-    console.log(status);
   }
+
+  // Функция для отправки запроса на сервер для проверки доступности никнейма
+  async checkNicknameAvailability(nickname) {
+    try {
+      const response = await fetch(`${this.apiUrl}/check-nickname/?nickname=${nickname}`, {
+        method: 'POST',
+      });
+
+      if (!response.ok) {
+        throw new Error('Ошибка взаимодейтсвия с сервером');
+      }
+
+      const data = await response.json();
+      return data.available;
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+  }
+
+
 }
 
-const eventSource = new EventSource('http://localhost:7070/sse');
+
+
+const subscriptions = document.querySelector('.subscriptions');
+const eventSource = new EventSource('http://localhost:7070/sseUsers');
 
 eventSource.addEventListener('open', (e) => {
-  console.log(e);
-
+  // console.log(e);
   console.log('sse open');
 });
 
 eventSource.addEventListener('error', (e) => {
   console.log(e);
-
   console.log('sse error');
 });
 
-const subscriptions = document.querySelector('.subscriptions');
+eventSource.addEventListener('updateUser', (e) => {
+  console.log('message sse', e.data);
+  const nickNames = JSON.parse(e.data);
+  console.log('nickNames sse', nickNames);
 
-eventSource.addEventListener('message', (e) => {
-  console.log(e);
-  const { name, phone } = JSON.parse(e.data);
+  // subscriptions.appendChild(document.createTextNode(`${nickName}\n`));
 
-  subscriptions.appendChild(document.createTextNode(`${name} ${phone}\n`));
-
-  console.log('sse message');
 });
 
-const ws = new WebSocket('ws://http://localhost:7070/ws');
+
+
+
+const ws = new WebSocket('ws://localhost:7070/ws');
 
 const chat = document.querySelector('.chat-widget');
 const chatMessage = chat.querySelector('.chat-widget__messages');
@@ -154,44 +122,40 @@ const chatSend = chat.querySelector('.chat-widget__input');
 
 chatSend.addEventListener('click', () => {
   const message = chatMessage.value;
-
   if (!message) return;
 
   ws.send(message);
-
   chatMessage.value = '';
 });
 
 ws.addEventListener('open', (e) => {
-  console.log(e);
-
+  // console.log(e);
   console.log('ws open');
 });
 
 ws.addEventListener('close', (e) => {
   console.log(e);
-
   console.log('ws close');
 });
 
 ws.addEventListener('error', (e) => {
   console.log(e);
-
   console.log('ws error');
 });
 
+// прием сообщения
 ws.addEventListener('message', (e) => {
-  console.log(e);
-
+  console.log('ws message');
+  // console.log(e);
   const data = JSON.parse(e.data);
-  const { chat: messages } = data;
-
-  messages.forEach(message => {
-    chat.appendChild(document.createTextNode(message) + '\n');
+  console.log('Messages:');
+  data.forEach(d => {
+    const { time, user, message } = d;
+    console.log(message);
+    // chat.appendChild(document.createTextNode(message) + '\n');
   });
 
-  console.log('ws message');
 });
 
-window.api = new SubscriptionApi('http://localhost:7070/');
+window.api = new ChatApi('http://localhost:7070');
 
