5f3617f88f782df4704781aa8699394f2d08307c
unker <kfi.unknown@gmail.com>
2023-09-13 22:50:37 +0300

    ws is replaced into class

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,4 @@
-// const chatBtn = document.querySelector('.chat-widget__side')
-// const input = document.querySelector('.chat-widget__input');
-// const messages = document.querySelector('.chat-widget__messages');
-// const chatContainer = document.querySelector('.chat-widget__messages-container');
-
-// chatBtn.addEventListener('click', (e) => {
-//   chatWidget.classList.add('chat-widget_active');
-// });
-
-
-
-
-
 url = 'http://localhost:7070'
-
-
-
-
 
 
 class ChatApi {
@@ -23,11 +6,13 @@ class ChatApi {
     this.apiUrl = apiUrl;
     this.nickName = undefined;
 
-    this.chatWidget = document.querySelector('.chat-widget');
-    this.inputChat = document.querySelector('.chat-widget__input');
-    this.messages = document.querySelector('.chat-widget__messages');
+    // элементы чата
+    this.chat = document.querySelector('.chat-widget');
+    this.inputMsg = document.querySelector('.chat-widget__input');
+    this.chatMessage = document.querySelector('.chat-widget__messages');
     this.chatContainer = document.querySelector('.chat-widget__messages-container');
 
+    // элементы моального окна с вводом никнейма
     this.modal = document.querySelector('.modal');
     this.nicknameInput = modal.querySelector('.nickname-input');
     this.submitButton = modal.querySelector('.nickname-submit');
@@ -35,16 +20,67 @@ class ChatApi {
 
     this.#getUserNameModal();
 
-    // this.inputChat.addEventListener('keydown', (e) => {
-    //   if (e.key === 'Enter' && e.repeat === false) {
-    //     const msg = this.inputChat.value.trim();
-    //     if (msg) {
-    //       // отправим сообщение и очистим поле ввода
-    //       sendMsg(msg, true);
-    //       this.inputChat.value = '';
-    //     }
-    //   }
-    // });
+    this.ws = new WebSocket(apiUrl.replace(/http/, 'ws') + '/ws');
+
+    this.#initWs(this.ws);
+  }
+
+  // отобразить принятое сообщение в чате
+  showMsg(msg, time, user) {
+    const isMyMsg = user === this.nickName;
+    const nickNameMsg = isMyMsg ? 'You' : user;
+    this.chatMessage.innerHTML += `
+      <div class="message ${isMyMsg ? 'message_client' : ''}">
+        <div class="message__time">${nickNameMsg}, ${time}</div>
+        <div class="message__text">${msg}</div>
+      </div>
+    `
+    this.scrollToBottom();
+  }
+
+  // прокрутку окна чата до блока последнего комментария
+  scrollToBottom() {
+    this.chatContainer.scrollTop = this.chatContainer.scrollHeight;
+  }
+
+  #initWs(ws) {
+    // отправка сообщений
+    this.inputMsg.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' && e.repeat === false) {
+        const msg = this.inputMsg.value.trim();
+        if (msg) {
+          const data = {
+            message: msg,
+            nickName: this.nickName,
+          }
+          ws.send(JSON.stringify(data));
+          this.inputMsg.value = '';
+        }
+      }
+    });
+
+    // прием сообщения
+    ws.addEventListener('message', (e) => {
+      const data = JSON.parse(e.data);
+      console.log('Messages:', data);
+      data.forEach(d => {
+        const { time, user, message } = d;
+        console.log(time, user, message);
+        this.showMsg(message, time, user)
+      });
+    });
+
+    ws.addEventListener('open', (e) => {
+      console.log('ws open');
+    });
+
+    ws.addEventListener('close', (e) => {
+      console.log('ws close');
+    });
+
+    ws.addEventListener('error', (e) => {
+      console.log('ws error');
+    });
   }
 
   #getUserNameModal() {
@@ -68,7 +104,7 @@ class ChatApi {
         // Если никнейм доступен, открываем окно чата
         this.errorText.style.display = 'none';
         this.modal.style.display = 'none';
-        this.chatWidget.classList.add('chat-widget_active');
+        this.chat.classList.add('chat-widget_active');
         this.nickName = nickname;
       } else {
         // Иначе сообщаем пользователю, что никнейм занят
@@ -77,7 +113,6 @@ class ChatApi {
       }
 
     });
-
   }
 
   // Функция для отправки запроса на сервер для проверки доступности никнейма
@@ -104,7 +139,6 @@ class ChatApi {
 
 
 
-const subscriptions = document.querySelector('.subscriptions');
 const eventSource = new EventSource('http://localhost:7070/sseUsers');
 
 eventSource.addEventListener('open', (e) => {
@@ -122,89 +156,8 @@ eventSource.addEventListener('updateUser', (e) => {
   const nickNames = JSON.parse(e.data);
   console.log('nickNames sse', nickNames);
 
-  // subscriptions.appendChild(document.createTextNode(`${nickName}\n`));
-
 });
 
 
-
-
-const ws = new WebSocket('ws://localhost:7070/ws');
-
-const chat = document.querySelector('.chat-widget');
-const chatMessage = chat.querySelector('.chat-widget__messages');
-const inputMsg = chat.querySelector('.chat-widget__input');
-const chatContainer = document.querySelector('.chat-widget__messages-container');
-
-
-inputMsg.addEventListener('keydown', (e) => {
-  // const message = chatMessage.value;
-  // if (!message) return;
-  console.log('inputMsg event');
-  if (e.key === 'Enter' && e.repeat === false) {
-    const msg = inputMsg.value.trim();
-    if (msg) {
-      // отправим сообщение и очистим поле ввода
-      data = {
-        message: msg,
-        nickName: 'asd123', // todo set
-      }
-      ws.send(JSON.stringify(data));
-      inputMsg.value = '';
-    }
-  }
-
-  // ws.send(message);
-  // chatMessage.value = '';
-});
-
-ws.addEventListener('open', (e) => {
-  // console.log(e);
-  console.log('ws open');
-});
-
-ws.addEventListener('close', (e) => {
-  console.log(e);
-  console.log('ws close');
-});
-
-ws.addEventListener('error', (e) => {
-  console.log(e);
-  console.log('ws error');
-});
-
-// прием сообщения
-ws.addEventListener('message', (e) => {
-  console.log('ws message');
-  // console.log(e);
-  const data = JSON.parse(e.data);
-  console.log('Messages:');
-  data.forEach(d => {
-    const { time, user, message } = d;
-    console.log(time, user, message);
-    showMsg(message, time, user)
-    // chat.appendChild(document.createTextNode(message) + '\n');
-
-  });
-});
-
-// отправить сообщение в чат
-function showMsg(msg, time, user) {
-  const isMyMsg = user === 'asd123';//this.nickName;
-  nickName = isMyMsg ? 'You' : user;
-  chatMessage.innerHTML += `
-  <div class="message ${isMyMsg ? 'message_client' : ''}">
-    <div class="message__time">${nickName}, ${time}</div>
-    <div class="message__text">${msg}</div>
-  </div>
-  `
-  scrollToBottom();
-}
-
-// прокрутку окна чата до блока последнего комментария
-function scrollToBottom() {
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-}
-
-window.api = new ChatApi('http://localhost:7070');
+window.api = new ChatApi(url);
 
